d2c0165b2db9066717d46eda299d8fe3807c2786
Shohanur-Shohan <shohanurshohan734@gmail.com>
2023-07-04 19:50:11 +0600

    done

---
+++ products.js
@@ -0,0 +1,48 @@
+const products = [
+    {
+        id: 1,
+        name: "ULTRABOOST SHOES",
+        price: 200,
+        stock: 23,
+        image: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/fbaf991a78bc4896a3e9ad7800abcec6_9366/Ultraboost_22_Shoes_Black_GZ0127_01_standard.jpg"
+    },
+    {
+        id: 2,
+        name: "NEW DNA SHOES",
+        price: 750,
+        stock: 7,
+        image: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/2cee64414e1f4f31baf1ae270099d950_9366/Lunar_New_Year_Ultraboost_DNA_Shoes_Black_GZ6074_01_standard.jpg"
+    },
+    {
+        id: 3,
+        name: "SUPERNOVA SHOES",
+        price: 950,
+        stock: 8,
+        image: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/69cbc73d0cb846889f89acbb011e68cb_9366/Supernova_Shoes_Black_S42722_01_standard.jpg"
+    },
+    {
+        id: 4,
+        name: "LITE RACER 3.0 SHOES",
+        price: 650,
+        stock: 15,
+        image: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/96a5f085fedf4e678095abad01056711_9366/Lite_Racer_Adapt_3.0_Shoes_Black_FX8802_01_standard.jpg"
+    },
+    {
+        id: 5,
+        name: "4DFWD SHOES",
+        price: 700,
+        stock: 25,
+        image: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8c5d1994dfd343e28567ad4400dd351d_9366/4DFWD_Shoes_Black_Q46447_01_standard.jpg"
+    },
+    {
+        id: 6,
+        name: "ADDIDAS CAP",
+        price: 700,
+        stock: 10,
+        image: "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/0ce5f49aa0914deabf16adc600d09e60_9366/Baseball_Cap_Black_HD7039_01_standard.jpg"
+    }
+]
+
+
+
+// export default products;
